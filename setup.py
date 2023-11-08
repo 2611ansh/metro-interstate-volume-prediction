2172a7af194be0443795751e9824d8b4c6361a63
Ansh Raghuvanshi <121013240+2611ansh@users.noreply.github.com>
2023-11-08 14:18:29 +0530

    setup and requirements done

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+hypen="-e."
+def get_requirements(file_path:str):
+    with open(file_path) as file:
+        lines=file.readlines()
+        requirements=[line.split("\n")[0] for line in lines]
+        if hypen in requirements:
+            requirements.remove(hypen)
+    return requirements
+
+
+setup(
+    name="metro interstate",
+    version="0.0.1",
+    author="Ansh",
+    author_email="anshjabalpur@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
